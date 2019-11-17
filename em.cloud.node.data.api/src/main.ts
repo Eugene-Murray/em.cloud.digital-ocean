f04c4bfff9c1f24d952ef234bffca9357d8ce9bd
Eugene Murray <eugene.murray@outlook.com>
2019-11-17 22:03:57 +0000

    add swagger

--- em.cloud.node.data.api/src/main.ts
+++ em.cloud.node.data.api/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ApplicationModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('em.cloud.solutions/api')
+    .setDescription('em.cloud.solutions API description')
+    .setVersion('1.0')
+    .addTag('data-api')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3001);
 }
 bootstrap();
