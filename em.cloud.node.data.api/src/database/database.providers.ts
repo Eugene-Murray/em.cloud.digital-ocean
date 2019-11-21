9f17d23d3dada2bba684bfb7ce0c4229fed61133
Eugene Murray <eugene.murray@gmail.com>
2019-11-21 21:43:03 +0000

    update mongoDB

--- em.cloud.node.data.api/src/database/database.providers.ts
+++ em.cloud.node.data.api/src/database/database.providers.ts
@@ -5,6 +5,6 @@ export const databaseProviders = [
     provide: 'DATABASE_CONNECTION',
     useFactory: async (): Promise<typeof mongoose> =>
       //await mongoose.connect('mongodb://localhost/nest'),
-      await mongoose.connect('mongodb+srv://cms-user:thedoors@cluster0-v83pi.azure.mongodb.net/eugene?retryWrites=true&w=majority'),
+      await mongoose.connect('mongodb+srv://cms-user:thedoors@cluster0-v83pi.azure.mongodb.net/digital-ocean?retryWrites=true&w=majority'),
   },
 ];
