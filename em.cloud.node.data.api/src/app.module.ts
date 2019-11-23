2d5ae38081261c37403f898532b04795b26af4a3
Eugene Murray <eugene.murray@gmail.com>
2019-11-23 20:28:00 +0000

    add dashboard module

--- em.cloud.node.data.api/src/app.module.ts
+++ em.cloud.node.data.api/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { SitesModule } from './sites/sites.module';
 import { WebHooksModule } from './web-hooks/web-hooks.module';
+import { DashboardsModule } from './dashboards/dashboards.module';
 
 @Module({
-  imports: [SitesModule, WebHooksModule],
+  imports: [SitesModule, WebHooksModule, DashboardsModule],
 })
 export class ApplicationModule {}
