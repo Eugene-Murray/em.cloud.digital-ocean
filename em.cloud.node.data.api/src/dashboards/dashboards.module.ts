5cdbf70a81c2f646c610f8db989bf5f8c600f1a7
Eugene Murray <eugene.murray@gmail.com>
2019-11-23 20:41:17 +0000

    add dashboard DB provider

--- em.cloud.node.data.api/src/dashboards/dashboards.module.ts
+++ em.cloud.node.data.api/src/dashboards/dashboards.module.ts
@@ -3,11 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { DashboardController } from './dashboard.controller';
 import { DashboardService } from './dashboard.service';
 import { WidgetSchema } from './schemas/widget.schema';
+import { DatabaseModule } from '../database/database.module';
+import { dashboardProviders } from './dashboard.providers';
 
 @Module({
-    imports: [MongooseModule.forFeature([{ name: 'Widget', schema: WidgetSchema }])],
+    imports: [DatabaseModule, MongooseModule.forFeature([{ name: 'Widget', schema: WidgetSchema }])],
     controllers: [DashboardController],
-    providers: [DashboardService],
+    providers: [DashboardService, ...dashboardProviders],
 })
 export class DashboardsModule {
 }
